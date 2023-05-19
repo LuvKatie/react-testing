ace3cbfd507495eb5101e75fb468f47346293868
Tony W <bobacrypto2@gmail.com>
2023-05-19 03:21:11 -0700

    Use logTestingPlaygroundURL for debugging tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ import App from "./App";
 describe("App component", () => {
     it("renders correct heading", () => {
         render(<App />);
+        screen.logTestingPlaygroundURL(screen.getByText('Our First Test'));
         expect(screen.getByRole("heading", { name: "Our First Test" }).textContent).toMatch(/our first test/i)
     })
 })
