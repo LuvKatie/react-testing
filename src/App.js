242d922d9df2c245d37810257eac483a93706340
Tony W <bobacrypto2@gmail.com>
2023-05-19 02:51:56 -0700

    Setup first test in React and successfully pass it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import {render} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+// import TestingComponent from "path-to-test-component";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
-  );
+    <h1>Our First Test</h1>
+  )
 }
 
 export default App;
